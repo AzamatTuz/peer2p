5e4e18e59ca1690aa08c7202ec82b60b90ab39b1
Azamat <tuzelbajazamat361@gmail.com>
2025-07-28 11:06:32 +0500

    changes

--- src/components/ProfileMain.jsx
+++ src/components/ProfileMain.jsx
@@ -12,7 +12,8 @@ function ProfileMain() {
     const [profileImage, setProfileImage] = useState()
     const [user, setUser] = useState({})
     const [isClick, setIsClick] = useState(false)
-
+    const [rentBtn, setRentBtn] = useState(true)
+    const [userId, setUserId] = useState()
 
     const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -46,8 +47,7 @@ function ProfileMain() {
     }
   };
   
-  console.log(user.phone_number);
-  
+
 
     async function getUser() {
         try {
@@ -57,6 +57,7 @@ function ProfileMain() {
                 },
             })
             setUser(result.data[0]);
+            setUserId(result.data[0].id)
             
         } catch (error) {
             console.error(error);
@@ -64,17 +65,54 @@ function ProfileMain() {
         }
     }
 
+    async function getProducts() {
+        try {
+
+            const result = await axios.get(`http://localhost:3000/products`, {userId})
+            console.log(result.data);
+            
+        } catch (error) {
+            console.error(error);
+            
+        }
+    }
+
+    if (user) {
+      getProducts()
+    }
+
     useEffect(()=>{
         getUser()
+        
     },[])
+
+    
   return (
     
-    <main className='shadowed mt-80 border-t-10 border-t-[#ADC178] flex justify-between'>
-        <ChangeProfileImage setPreview={setPreview} profileImage={profileImage} user={user} handleImageChange={handleImageChange} preview={preview} handleSubmit={handleSubmit}/>
+    <main className='shadowed mt-80 border-t-10 border-t-[#ADC178] flex flex-col justify-between'>
+        <section className='flex justify-between'>
+          <ChangeProfileImage setPreview={setPreview} profileImage={profileImage} user={user} handleImageChange={handleImageChange} preview={preview} handleSubmit={handleSubmit}/>
 
-        <UserInfo user={user} setIsClick={setIsClick} isClick={isClick}/>
+          <UserInfo user={user} setIsClick={setIsClick} isClick={isClick}/>
+        </section>
+
+        <section>
+          <article className='flex gap-3 ml-5'>
+            <h3 className='text-2xl font-medium'>Жалға</h3>
+            <button onClick={() => setRentBtn(true)} className={`${rentBtn && 'bg-[#ADC178] text-white'} pl-3 pr-3 text-2xl rounded-t-2xl cursor-pointer`}>Берген</button>
+            <button onClick={() => setRentBtn(false)} className={`${rentBtn || 'bg-[#ADC178] text-white'} pl-3 pr-3 text-2xl rounded-t-2xl cursor-pointer`}>Алған</button>
+            <h3 className='text-2xl'>заттар</h3>
+          </article>
+
+          <hr className='w-full h-2 border-none bg-[#ADC178]'/>  
+
+        </section>
 
         {isClick && <VerificationUser setIsClick={setIsClick} isClick={isClick} id={user.id}/>}
+
+        {rentBtn ? <section>
+
+        </section> : ''}
         
     </main>
   )
